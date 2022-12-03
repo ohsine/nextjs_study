9d4db899354bd246dd873a6df868a618cf08cbb7
ohsine <ohsine@korea.ac.kr>
2022-12-03 20:20:15 +0900

    1203_
    next.js Route-Link

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -7,29 +7,29 @@ export default function ItemList({list}) {
     <div>
         <Grid columns={3} divided>
             <Grid.Row>
-        {list.map((item) => (
-                <Grid.Column key={item.id}>
-                    <Link href={`/view/${item.id}`}>
+                {list.map((item) => (
+                    <Grid.Column key={item.id}>
+                        <Link href={`/view/${item.id}`}>
 
-                        <div className={styles.wrap}>
+                            <div className={styles.wrap}>
 
-                            <img
-                            src={item.image_link}
-                            alt={item.name}
-                            className={styles.img_item}
-                            />
+                                <img
+                                src={item.image_link}
+                                alt={item.name}
+                                className={styles.img_item}
+                                />
 
-                            <strong className={styles.tit_item}>{item.name}</strong>
-                            <span className={styles.txt_info}>
-                                {item.category} {item.product_type}
-                            </span>
-                            <strong className={styles.num_price}>${item.price}</strong>
+                                <strong className={styles.tit_item}>{item.name}</strong>
+                                <span className={styles.txt_info}>
+                                    {item.category} {item.product_type}
+                                </span>
+                                <strong className={styles.num_price}>${item.price}</strong>
 
-                        </div>
+                            </div>
 
-                    </Link>
-                </Grid.Column>
-            ))}
+                        </Link>
+                    </Grid.Column>
+                ))}
 
             </Grid.Row>
         </Grid>
